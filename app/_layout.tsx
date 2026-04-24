0e7f51cf2e82d0180f79ec1aba2468f588791aaf
karl-ha <karl.soderholm@ha.ax>
2026-04-24 20:58:22 +0300

    Började fixa med API:n

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,53 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  const [kpValue, setKpValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchAuroraData() {
+      try {
+        // Mariehamns koordinater
+        const lat = 60.093;
+        const long = 19.939;
+
+        const response = await fetch(`https://auroras.live${lat}&long=${long}`);
+        const data = await response.json();
+
+        setKpValue(data.ace.kp);
+        setProbability(data.probability);
+
+        console.log("Got data", data);
+      } catch (error) {
+        console.error("API did not work", error);
+      }
+    }
+    fetchAuroraData();
+  }, []);
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
